952c283987de7ebc72490f5b49d71ad1f4eb968e
Dmitriy Shedko <theegotunnel@gmail.com>
2023-03-21 10:45:00 +0300

    feat: add menu imgs Tue Mar 21 10:45:00 BST 2023

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('mobile-menu');
+const btnHamburgerT = document.querySelector('.hamburger-top');
+const btnHamburgerM = document.querySelector('.hamburger-middle');
+const btnHamburgerB = document.querySelector('.hamburger-bottom');
 
 btn.addEventListener('click', navToggle);
 
 function navToggle() {
   btn.classList.toggle('open');
   menu.classList.toggle('show-menu');
-  btn.classList.toggle('black');
+  btnHamburgerT.classList.toggle('black');
+  btnHamburgerM.classList.toggle('black');
+  btnHamburgerB.classList.toggle('black');
 }
